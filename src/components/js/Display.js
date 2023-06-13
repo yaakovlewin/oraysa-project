4c2bccb6639b0710f51f7ade6a1e35c5d93f9f3b
yaakov lewin <ynslewin@gmail.com>
2023-06-13 21:54:58 +0100

    optimised letter to number

--- src/components/js/Display.js
+++ src/components/js/Display.js
@@ -2,7 +2,7 @@ export default function Display({ hebDate, gregorianDate }) {
     return (
         <section className='flex py-8 justify-around '>
             {/* <p className='text-sky-800 '>Selected: <em>{selectedOption1} דף {selectedOption2} עמוד {selectedAmud ? 'ע"א' : 'ע"ב'}</em></p> */}
-            <p className='text-sky-800'>Hebrew Date: {hebDate}</p>
+            <p className='font-semibold text-md text-sky-600'>Hebrew Date: {hebDate}</p>
             <p className='text-sky-800'>Gregorian Date: {gregorianDate}</p>
         </section>
     )
--- src/js/letter-to-num.js
+++ src/js/letter-to-num.js
@@ -1,45 +1,41 @@
-let numbers = [
-    { letter: '', num: 0 },
-    { letter: 'א', num: 1 },
-    { letter: 'ב', num: 2 },
-    { letter: 'ג', num: 3 },
-    { letter: 'ד', num: 4 },
-    { letter: 'ה', num: 5 },
-    { letter: 'ו', num: 6 },
-    { letter: 'ז', num: 7 },
-    { letter: 'ח', num: 8 },
-    { letter: 'ט', num: 9 },
-    { letter: 'י', num: 10 },
-    { letter: 'כ', num: 20 },
-    { letter: 'ל', num: 30 },
-    { letter: 'מ', num: 40 },
-    { letter: 'נ', num: 50 },
-    { letter: 'ס', num: 60 },
-    { letter: 'ע', num: 70 },
-    { letter: 'פ', num: 80 },
-    { letter: 'צ', num: 90 },
-    { letter: 'ק', num: 100 },
-    { letter: 'ר', num: 200 },
-    { letter: 'ש', num: 300 },
-    { letter: 'ת', num: 400 },
-    { letter: 'תק', num: 500 },
-    { letter: 'תר', num: 600 },
-    { letter: 'תש', num: 700 },
-    { letter: 'תת', num: 800 },
-    { letter: 'תתק', num: 900 },
-    { letter: 'תתר', num: 1000 },
-]
-
+const numbers = {
+    '': 0,
+    'א': 1,
+    'ב': 2,
+    'ג': 3,
+    'ד': 4,
+    'ה': 5,
+    'ו': 6,
+    'ז': 7,
+    'ח': 8,
+    'ט': 9,
+    'י': 10,
+    'כ': 20,
+    'ל': 30,
+    'מ': 40,
+    'נ': 50,
+    'ס': 60,
+    'ע': 70,
+    'פ': 80,
+    'צ': 90,
+    'ק': 100,
+    'ר': 200,
+    'ש': 300,
+    'ת': 400,
+    'תק': 500,
+    'תר': 600,
+    'תש': 700,
+    'תת': 800,
+    'תתק': 900,
+    'תתר': 1000
+};
 
 function numerize(letter) {
     let letterNum = 0;
     for (let i = 0; i < letter.length; i++) {
-        for (let j = 0; j < numbers.length; j++) {
-            if (letter.charAt(i) === numbers[j].letter) {
-                letterNum += numbers[j].num;
-            }
-        }
+        letterNum += numbers[letter.charAt(i)] || 0;
     }
     return letterNum;
 }
+
 export default numerize;
