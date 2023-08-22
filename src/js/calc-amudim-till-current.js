b45573a7282fed747f951310604842e8527b624d
yanki levine <70327894+yaakovlewin@users.noreply.github.com>
2023-08-22 07:38:50 +0100

    removed console logs

--- src/js/calc-amudim-till-current.js
+++ src/js/calc-amudim-till-current.js
@@ -1,15 +1,11 @@
 import masechtot from "./data.js";
 export default function getAmudim(maseches, daf, amud) {
-    console.log(`maseches: ${maseches}, daf: ${daf}, amud: ${amud}`);
     // finds the index of the current maseches in the masechtot array
     const masechesIndex = masechtot.findIndex(element => element.name === maseches);
     // calculates the total amudim till the current amud by accumulating the amudim of the previous masechtot and the current maseches minus 1 (so first page of berachos is set to be day 0)
     const currentMasechesAmudim = ((daf - 1) * 2) + amud - 1;
     const previousMasechesAmudim = masechtot.slice(0, masechesIndex).reduce((total, { pages }) => total + (pages * 2), 0) - 2;
     const totalAmudim = currentMasechesAmudim + (maseches !== 'ברכות' ? previousMasechesAmudim : 0);
-    console.log(`totalAmudim: ${totalAmudim}`);
-    console.log(`Current Maseches amudim: ${currentMasechesAmudim}`);
-    console.log(`Previous Maseches dafim: ${previousMasechesAmudim}`);
     return totalAmudim;
 }
 
--- src/js/getGeorgienDate.js
+++ src/js/getGeorgienDate.js
@@ -8,7 +8,6 @@ function addDaysToStartDate(date, days) {
 function addNonWorkingDays(totalWorkingDays, workingDaysOfWeek, startingDate) {
     let fullWeeks = Math.floor(totalWorkingDays / workingDaysOfWeek) * 7
     let remainingDays = totalWorkingDays % workingDaysOfWeek;
-    console.log(`remainingDays: ${remainingDays}`)
 
 
     // if odd days of full weeks roll over to a new week add 2 days.
@@ -19,14 +18,11 @@ function addNonWorkingDays(totalWorkingDays, workingDaysOfWeek, startingDate) {
 }
 
 export default function calculateDate(activeDaysSum, startDate) {
-    console.log(`activeDaysSum: ${activeDaysSum}`)
     const startingDate = new Date(startDate);
     let workingDaysOfWeek = 5
     let daysToAdd = addNonWorkingDays(activeDaysSum, workingDaysOfWeek, startingDate)
-    console.log(daysToAdd)
     let date = addDaysToStartDate(startingDate, daysToAdd)
     let dateStr = date.toISOString().slice(0, 10);
-    console.log(dateStr)
     let day = date.getDay()
     return { dateStr, day }
 }
