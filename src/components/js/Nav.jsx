0d4677e30acf5ddddb3f0674b51694f48952668f
yanki levine <70327894+yaakovlewin@users.noreply.github.com>
2023-09-03 18:07:37 +0100

    added hight and width to img

--- src/components/js/Nav.jsx
+++ src/components/js/Nav.jsx
@@ -6,7 +6,9 @@ export default function Nav() {
             <img
                 alt="logo"
                 src={logo}
-                className=" m-auto my-1 md:pl-24 md:ml-20"
+                className="p-2 m-auto my-1 md:pl-24 md:ml-20"
+                height={88}
+                width={200}
             ></img>
         </nav>
     );
